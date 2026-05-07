4c411a6eeefbf0b9a287557010a6c9ca20f23516
ISABELE VITÓRIA PIRES <isabele.pires@univille.br>
2026-05-07 20:19:02 -0300

    adicionando mais 4 novas functions

--- src/function_app.py
+++ src/function_app.py
@@ -7,7 +7,15 @@ app = func.FunctionApp()
 from triggers.extract_cliente import app as extract_cliente
 from triggers.extract_pedido import app as extract_pedido
 from triggers.extract_entrega import app as extract_entrega
+from triggers.extract_produto import app as extract_produto
+from triggers.extract_regiao import app as extract_regiao
+from triggers.extract_representante import app as extract_representante
+from triggers.extract_transportadora import app as extract_transportadora
 
 app.register_functions(extract_cliente)
 app.register_functions(extract_pedido)
 app.register_functions(extract_entrega)
+app.register_functions(extract_produto)
+app.register_functions(extract_regiao)
+app.register_functions(extract_representantes)
+app.register_functions(extract_transportadora)
