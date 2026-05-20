53f8da390671451698da269e5bf09032a811dbb6
ISABELE VITÓRIA PIRES <isabele.pires@univille.br>
2026-05-20 20:23:23 -0300

    alteração de variaveis de ambiente

--- src/triggers/extract_categoria_produto.py
+++ src/triggers/extract_categoria_produto.py
@@ -9,11 +9,11 @@ app = func.Blueprint()
 def extract_categoria_produto(timer: func.TimerRequest) -> None:
 
     sql_server = os.getenv("SQL_SERVER_SOURCE")
-    database = os.getenv("SQL_DATABASE_SOURCE")
+    sql_database = os.getenv("SQL_DATABASE_SOURCE")
     sql_user = os.getenv("SQL_USER_SOURCE")
     sql_pass = os.getenv("SQL_PASSWORD_SOURCE")
 
-    logging.info(f"sql_server:{sql_server}, database:{database}, user:{sql_user}, password:{sql_pass}...")
+    logging.info(f"sql_server:{sql_server}, database:{sql_database}, user:{sql_user}, password:{sql_pass}...")
 
     # Configura a string de conexão para o banco de dados SQL Server
     conn_str = (
