7a86b35b1565f49c9f94b334abd842b3d0f75235
ISABELE VITÓRIA PIRES <isabele.pires@univille.br>
2026-05-27 20:31:09 -0300

    função adicionada na function.app

--- src/function_app.py
+++ src/function_app.py
@@ -5,7 +5,7 @@ app = func.FunctionApp()
 
 # Importa triggers para registrar as functions no app
 from triggers.extract_cliente import app as extract_cliente
-from triggers.extract_cliente import app as extract_cliente2
+from triggers.extract_cliente2 import app as extract_cliente2
 from triggers.extract_pedido import app as extract_pedido
 from triggers.extract_entrega import app as extract_entrega
 from triggers.extract_produto import app as extract_produto
